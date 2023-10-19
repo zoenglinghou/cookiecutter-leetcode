c29dda11eb7c9f6f774b143ceb893ebf271e0a62
Linghao Zhang <linghao.zhang@protonmail.com>
2023-10-19 14:15:27 +0200

    Update template

--- {{ cookiecutter.project_slug }}/main.cpp
+++ {{ cookiecutter.project_slug }}/main.cpp
@@ -1,59 +1,15 @@
 #include <gtest/gtest.h>
 
-
 using namespace std;
 
 class Solution {
 public:
-  {{cookiecutter.output_class}} {{ cookiecutter.method_name }} (
-  {%- for _, details in cookiecutter.inputs | dictsort -%}
-    {%- if details.class is not None -%}
-      {{ details.class }} {{ detilas.identifier }},
-    {%- endif -%}
-  {%- endfor -%}
-) {
- 
-  }
 };
+class MainTest : public ::testing::TestWithParam<tuple<>> {};
 
-class MainTest :
-  public ::testing::TestWithParam<tuple<
-  {%- for _, details in cookiecutter.inputs | dictsort -%}
-    {%- if details.class is not None -%}
-      {{ details.class }},
-    {%- endif -%}
-  {%- endfor -%}
-  {{- cookiecutter.output_class -}}>> {};
+TEST_P(MainTest, Solution){};
 
-TEST_P(MainTest, romanToInt) {
-  Solution solution;
-  {% for _, details in cookiecutter.inputs | dictsort -%}
-    {%- if details.class is not None -%}
-      {{ details.class }} {{ detilas.identifier }};
-    {%- endif -%}
-  {%- endfor %}
-  {{ cookiecutter.output_class }} expected;
-  tie(
-    {%- for _, details in cookiecutter.inputs | dictsort -%}
-      {%- if details.class is not None -%}
-        {{ detilas.identifier }},
-      {%- endif -%}
-    {%- endfor -%}
-  expected) = GetParam();
-  ASSERT_EQ(expected, solution.{{ cookiecutter.method_name }}(
-    {%- for _, details in cookiecutter.inputs | dictsort -%}
-      {%- if details.class is not None -%}
-        {{ detilas.identifier }},
-      {%- endif -%}
-    {%- endfor -%}
-  ));
-};
-
-INSTANTIATE_TEST_SUITE_P(
-  MainTest, MainTest,
-  ::testing::Values(
-  )
-);
+INSTANTIATE_TEST_SUITE_P(MainTest, MainTest, ::testing::Values());
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
